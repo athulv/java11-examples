44856fdd72bc1c6bb6747c921c4300b241127d35
athulv <athul.03@rediffmail.com>
2025-07-22 14:28:03 +0000

    App java modification

--- src/main/java/io/github/biezhi/App.java
+++ src/main/java/io/github/biezhi/App.java
@@ -1,9 +1,8 @@
 package io.github.biezhi;
 
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Demo run of java11-examples project");
-        // For example, run one of the examples:
+    public static void main(String[] args) throws Exception {
+        System.out.println("Docker run: java11-examples starting...");
         io.github.biezhi.java11.http.Example.main(args);
     }
 }
