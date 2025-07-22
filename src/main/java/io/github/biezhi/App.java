8fda44a333ec58f002100c542aa770041dd54e6e
athulv <athul.03@rediffmail.com>
2025-07-22 14:22:31 +0000

    Add App entry point for Docker run

---
+++ src/main/java/io/github/biezhi/App.java
@@ -0,0 +1,10 @@
+package io.github.biezhi;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Demo run of java11-examples project");
+        // For example, run one of the examples:
+        io.github.biezhi.java11.http.Example.main(args);
+    }
+}
+
