4a60697aebde433efe7de4a70a1fd8a0ad0d0439
athulv <athul.03@rediffmail.com>
2025-07-22 15:54:57 +0000

    Added missing App.java to labtest-improvement

---
+++ src/main/java/io/github/biezhi/App.java
@@ -0,0 +1,9 @@
+package io.github.biezhi;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Docker run: java11-examples starting...");
+        io.github.biezhi.java11.http.Example.main(args);
+    }
+}
+
