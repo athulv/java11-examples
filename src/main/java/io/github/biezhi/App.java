5ef4e4360b82caf1db97f645e6645dad10076668
athulv <athul.03@rediffmail.com>
2025-07-22 17:08:22 +0000

    Updated App.java to print clean success message

--- src/main/java/io/github/biezhi/App.java
+++ src/main/java/io/github/biezhi/App.java
@@ -1,9 +1,8 @@
 package io.github.biezhi;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Docker run: java11-examples starting...");
-        io.github.biezhi.java11.http.Example.main(args);
+    public static void main(String[] args) {
+        System.out.println("Docker run: java11-examples running successfully!");
     }
 }
 
